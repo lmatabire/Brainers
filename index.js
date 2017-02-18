55265108a8c2051b0616ca32195d3ae4b51aa47f
lmatabire <Matabire23@gmail.com>
2017-02-17 21:24:35 -0800

    My Brain Eater Game

--- index.js
+++ index.js
@@ -1,46 +1,131 @@
-var c = document.getElementById('myCanvas');
-var context = c.getContext('2d');
-var img = new Image();
-var zX = 0;
-var zY = 0;
-img.onload = animate;
-img.src = "zombie.gif";
-img.addEventListener('load', function () {
-    context.drawImage(img, zX, zY);
-});
-document.addEventListener('keydown', function (event) {
-    switch (event.key) {
-        case 'ArrowUp':
-            moveUp();
-            break;
-        case 'ArrowDown':
-            moveDown();
-            break;
-        case 'ArrowLeft':
-            moveLeft();
-            break;
-        case 'ArrowRight':
-            moveRight();
-            break;
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Game = (function () {
+    function Game() {
+        this.c = document.getElementById('myCanvas');
+        this.context = this.c.getContext('2d');
     }
-}, true);
-function animate() {
-    context.clearRect(0, 0, c.width, c.height);
-    context.drawImage(img, zX, zY);
-}
-function moveUp() {
-    zY -= 10;
-    animate();
-}
-function moveDown() {
-    zY += 10;
-    animate();
-}
-function moveLeft() {
-    zX -= 10;
-    animate();
-}
-function moveRight() {
-    zX += 10;
-    animate();
-}
+    return Game;
+}());
+var Tile = (function () {
+    function Tile(image, x, y) {
+        var _this = this;
+        this.unit = 50;
+        this.image = new Image();
+        this.image.src = image;
+        this.image.height = this.unit;
+        this.image.width = this.unit;
+        this.w = this.unit;
+        this.h = this.unit;
+        this.x = x;
+        this.y = y;
+        this.image.addEventListener('load', function () {
+            _this.drawTile();
+        });
+    }
+    Tile.prototype.drawTile = function () {
+        game.context.drawImage(this.image, this.x, this.y, this.w, this.h);
+    };
+    Tile.prototype.clearTile = function () {
+        game.context.clearRect(this.x, this.y, this.w, this.h);
+    };
+    Tile.prototype.moveUp = function () {
+        if (this.canMoveImage(this.image, 'up')) {
+            this.clearTile();
+            this.y -= this.unit;
+            this.drawTile();
+        }
+    };
+    Tile.prototype.moveDown = function () {
+        if (this.canMoveImage(this.image, 'down')) {
+            this.clearTile();
+            this.y += this.unit;
+            this.drawTile();
+        }
+    };
+    Tile.prototype.moveLeft = function () {
+        if (this.canMoveImage(this.image, 'left')) {
+            this.clearTile();
+            this.x -= this.unit;
+            this.drawTile();
+        }
+    };
+    Tile.prototype.moveRight = function () {
+        if (this.canMoveImage(this.image, 'right')) {
+            this.clearTile();
+            this.x += this.unit;
+            this.drawTile();
+        }
+    };
+    Tile.prototype.canMoveImage = function (image, direction) {
+        switch (direction) {
+            case 'up':
+                return this.y > 0;
+            case 'down':
+                return (this.y + this.h) < game.c.height;
+            case 'left':
+                return this.x > 0;
+            case 'right':
+                return (this.x + this.w) < game.c.width;
+        }
+    };
+    return Tile;
+}());
+var Wall = (function (_super) {
+    __extends(Wall, _super);
+    function Wall(image, x, y) {
+        return _super.call(this, image, x, y) || this;
+    }
+    return Wall;
+}(Tile));
+var Zombie = (function (_super) {
+    __extends(Zombie, _super);
+    function Zombie(image, x, y) {
+        var _this = _super.call(this, image, x, y) || this;
+        console.log(_this);
+        return _this;
+    }
+    return Zombie;
+}(Tile));
+var Player = (function (_super) {
+    __extends(Player, _super);
+    function Player(image, x, y) {
+        var _this = _super.call(this, image, x, y) || this;
+        console.log(_this);
+        _this.createControls();
+        return _this;
+    }
+    Player.prototype.createControls = function () {
+        var _this = this;
+        document.addEventListener('keydown', function (event) {
+            switch (event.key) {
+                case 'ArrowUp':
+                    _this.moveUp();
+                    break;
+                case 'ArrowDown':
+                    _this.moveDown();
+                    break;
+                case 'ArrowLeft':
+                    _this.moveLeft();
+                    break;
+                case 'ArrowRight':
+                    _this.moveRight();
+                    break;
+            }
+        }, false);
+    };
+    return Player;
+}(Tile));
+var unit = 50;
+var game = new Game();
+var zombies = [
+    new Zombie('zombie.gif', unit, unit),
+    new Zombie('zombie.gif', unit * 1, unit * 2),
+    new Zombie('zombie1.gif', unit * 1, unit * 3),
+    new Zombie('zombie1.gif', unit * 3, unit * 2)
+];
+1;
+var player = new Player('player.png', 0, 0);
--- index.ts
+++ index.ts
@@ -1,52 +1,168 @@
-var c = <HTMLCanvasElement>document.getElementById('myCanvas');
-let context = c.getContext('2d');
-let img = new Image();
-var zX = 0;
-var zY = 0;
-img.onload = animate;
-img.src = "zombie.gif";
-img.addEventListener('load', () => {
-  context.drawImage(img, zX, zY);
-});
+//TODO measuring units in a grid
+//TODO generate random maze
+//TODO detect boundaries for user
+//TODO detect boundaries for zombies
+//TODO logic for zombie wandering
+//TODO end game on zombie contact
+//TODO OPTIONAL retrieving tincutures or cures or vaccines
 
-document.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'ArrowUp':
-      moveUp();
-      break;
-    case 'ArrowDown':
-      moveDown();
-      break;
-    case 'ArrowLeft':
-      moveLeft();
-      break;
-    case 'ArrowRight':
-      moveRight();
-      break;
+interface IGame {
+  c:HTMLCanvasElement;
+  context:CanvasRenderingContext2D;
+}
+
+interface ITile {
+  unit:number;
+  image:HTMLImageElement,
+  x:number,
+  y:number,
+  w:number,
+  h:number
+}
+
+class Game implements IGame{
+  c:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('myCanvas');
+  context:CanvasRenderingContext2D = this.c.getContext('2d');
+}
+
+class Tile implements ITile{
+  unit:number = 50;
+  image:HTMLImageElement;
+  x:number;
+  y:number;
+  w:number;
+  h:number;
+  constructor(
+    image:string,
+    x:number,
+    y:number
+  ) {
+    this.image = new Image();
+    this.image.src = image;
+    this.image.height = this.unit;
+    this.image.width = this.unit;
+    this.w = this.unit;
+    this.h = this.unit;
+    this.x = x;
+    this.y = y;
+    this.image.addEventListener('load', () => {
+      this.drawTile();
+    });
   }
-}, true);
 
-function animate() {
-  context.clearRect(0, 0, c.width, c.height);  // clear canvas
-  context.drawImage(img, zX, zY);
+  drawTile() {
+    game.context.drawImage(this.image, this.x, this.y, this.w, this.h);
+  }
+
+  clearTile() {
+    game.context.clearRect(this.x, this.y, this.w, this.h);
+  }
+
+  moveUp() {
+    if (this.canMoveImage(this.image, 'up')) {
+      this.clearTile();
+      this.y -= this.unit;
+      this.drawTile();
+    }
+  }
+
+  moveDown() {
+    if (this.canMoveImage(this.image, 'down')) {
+      this.clearTile();
+      this.y += this.unit;
+      this.drawTile();
+    }
+  }
+
+  moveLeft() {
+    if (this.canMoveImage(this.image, 'left')) {
+      this.clearTile();
+      this.x -= this.unit;
+      this.drawTile();
+    }
+  }
+
+  moveRight() {
+    if (this.canMoveImage(this.image, 'right')) {
+      this.clearTile();
+      this.x += this.unit;
+      this.drawTile();
+    }
+  }
+
+  canMoveImage(image:HTMLImageElement, direction:string) {
+    switch(direction) {
+      case 'up':
+        return this.y > 0;
+      case 'down':
+        return (this.y + this.h) < game.c.height;
+      case 'left':
+        return this.x > 0;
+      case 'right':
+        return (this.x + this.w) < game.c.width;
+    }
+  }
 }
 
-function moveUp() {
-  zY -= 10;
-  animate();
+
+class Wall extends Tile{
+  constructor(
+    image, x, y
+  )
+{
+  super(image, x, y);
+}
 }
 
-function moveDown() {
-  zY += 10;
-  animate();
+class Zombie extends Tile{
+  public image:HTMLImageElement
+  constructor(
+    image:string,
+    x:number,
+    y:number
+  ) {
+    super(image, x, y);
+    console.log(this);
+  }
 }
 
-function moveLeft() {
-  zX -= 10;
-  animate();
+class Player extends Tile{
+  constructor(
+    image:string,
+    x:number,
+    y:number
+  ) {
+    super(image, x, y);
+    console.log(this);
+    this.createControls();
+  }
+
+  createControls() {
+    document.addEventListener('keydown', (event) => {
+      switch (event.key) {
+        case 'ArrowUp':
+          this.moveUp();
+          break;
+        case 'ArrowDown':
+          this.moveDown();
+          break;
+        case 'ArrowLeft':
+          this.moveLeft();
+          break;
+        case 'ArrowRight':
+          this.moveRight();
+          break;
+      }
+    }, false);
+  }
 }
 
-function moveRight() {
-  zX += 10;
-  animate();
-}
+const unit = 50;
+let game:IGame = new Game();
+let zombies:Array<ITile> = [
+  new Zombie('zombie.gif', unit, unit),
+  new Zombie('zombie.gif', unit*1, unit*2),
+  new Zombie('zombie1.gif', unit*1, unit*3),
+  new Zombie('zombie1.gif', unit*3, unit*2)
+];1
+let player:ITile = new Player('player.png', 0, 0);
